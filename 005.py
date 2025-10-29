8bcf635e8a7047726ea6fbf40b2e990263bb5df1
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-10-29 12:05:55 +0100

    1

--- 005.py
+++ 005.py
@@ -1,20 +1,37 @@
 # Importamos libreria
 import sqlite3
-
 # Nos conectamos a la base de datos
-conexion = sqlite3.connect("empresas.db")
+conexion = sqlite3.connect("006.db")
 
-# Creamos un cursor
 cursor = conexion.cursor()
+#CRUD
 
-cursor.execute('''
-  SELECT * FROM clientes;
-''')
 
-filas = cursor.fetchall()
+      print("1.Leer")
+      print("2.Crear")
+      print("3.Editar")
+      print("4.Borrar")
 
-for fila in filas:
-  print(fila)
+      opcion = input("Elige")
 
-# Lanzamos la peticion
-conexion.commit()
+      if opcion == 1:
+            print("Lear")
+            # Creamos un cursor
+            
+
+            cursor.execute('''
+                  SELECT * FROM clientes;
+            ''')
+            filas = cursor.fetchall()
+
+            for fila in filas:    
+                  print(fila)
+
+            # Lanzamos la peticion
+            conexion.commit()
+      if opcion == 2:
+            print("Crear")
+      if opcion == 3:
+            print("Editar")
+      if opcion == 4:
+            print("4.Borrar")
