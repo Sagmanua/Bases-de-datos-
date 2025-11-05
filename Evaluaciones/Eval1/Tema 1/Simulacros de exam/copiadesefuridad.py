41807109f35d43844ee55c964aa0a824018aaccb
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-11-05 11:08:52 +0100

    Update copiadesefuridad.py

--- Evaluaciones/Eval1/Tema 1/Simulacros de exam/copiadesefuridad.py
+++ Evaluaciones/Eval1/Tema 1/Simulacros de exam/copiadesefuridad.py
@@ -12,7 +12,6 @@ try:
     print("✅ Conexión exitosa!")
     print("Hola tu redacta database portafolio")
     while True:
-        cursor = conexion.cursor()
         print("Escoge una opcion:")
         print("1.-Insertar un cliente")
         print("2.-Listar los clientes")
@@ -41,7 +40,7 @@ VALUES ('''+ Id_cat + ''',\'''' + titulo_c + '''\',\'''' + descripcion_c + '''\'
                 titulo_p = input("titulo_p VARCHAR(255): ")
                 descripcion_p = input("descripcion_p VARCHAR(255): ")
                 imagen = input("imagen: ")
-                tiurltulo_p = input("tiurltulo_p: ")
+                url = input("url VARCHAR")
                 id_categoria = input("id_categoria INT: ")
 
                 cursor.execute('''
@@ -51,7 +50,7 @@ VALUES ('''+ Id_cat + ''',\'''' + titulo_c + '''\',\'''' + descripcion_c + '''\'
                     "''' + titulo_p + '''",
                     "''' + descripcion_p + '''",
                     "''' + imagen + '''",
-                    "''' + tiurltulo_p + '''",
+                    "''' + url + '''",
                     '''+id_categoria+'''
                     );
                 ''')
@@ -65,6 +64,61 @@ VALUES ('''+ Id_cat + ''',\'''' + titulo_c + '''\',\'''' + descripcion_c + '''\'
             for fila in resultados:
                 print(fila)
         elif opcion == 3:
+            print("Escoge una opcion:")
+            print("1.Actulizar en tabla de Categoria")
+            print("2.Actulizar en tabla de Pieza")
+            opciondeinsetar = int(input("Escoge una opcion:"))
+            if opciondeinsetar == 1:
+                Id_cat = input("Id_cat INT: ")
+                titulo_c = input("titulo_c VARCHAR(255): ")
+                descripcion_c = input("descripcion_c VARCHAR(255): ")
+
+                cursor.execute('''
+                    UPDATE Categoria
+                    SET titulo_c = "'''+titulo_c+'''",
+                        descripcion_c = "'''+descripcion_c+'''"
+                    WHERE Id_cat = '''+Id_cat+'''
+                ''')
+                conexion.commit()
+            if opciondeinsetar == 2:
+                id_pieza = input("id_pieza INT: ")
+                titulo_p = input("titulo_p VARCHAR(255): ")
+                descripcion_p = input("descripcion_p VARCHAR(255): ")
+                imagen = input("imagen: ")
+                tiurltulo_p = input("tiurltulo_p: ")
+                url = input("url VARCHAR")
+                id_categoria = input("id_categoria INT: ")
+                cursor.execute('''
+                    UPDATE Pieza
+                    SET titulo_p = "'''+titulo_p+'''",
+                        descripcion_p = "'''+descripcion_p+'''",
+                        imagen = "'''+imagen+'''",
+                        url = "'''+url+'''",
+                        id_categoria = '''+id_categoria+'''
+                    WHERE id_pieza = '''+id_pieza+'''
+                ''')
+        elif opcion == 4:
+            print("Escoge una opcion:")
+            print("1.Actulizar en tabla de Categoria")
+            print("2.Actulizar en tabla de Pieza")
+            opciondeinsetar = int(input("Escoge una opcion:"))
+            if opciondeinsetar == 1:
+                print("Eliminamos un ")
+                id = input("Introduce el id del cliente que quieres eliminar: ")
+                cursor.execute('''
+                DELETE FROM Categoria
+                WHERE Identificador = '''+id+'''
+                ''')
+                
+                conexion.commit()
+            if opciondeinsetar == 2:
+                print("Eliminamos un ")
+                id = input("Introduce el id del cliente que quieres eliminar: ")
+                cursor.execute('''
+                DELETE FROM Pieza
+                WHERE Identificador = '''+id+'''
+                ''')
+        else:
             break
     cursor.close() 
     conexion.close()
