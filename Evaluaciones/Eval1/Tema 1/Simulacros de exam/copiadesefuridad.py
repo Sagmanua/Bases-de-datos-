13b86e839c271fedf43bdf7f23fce70af95798a5
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-11-05 10:50:22 +0100

    Update copiadesefuridad.py

--- Evaluaciones/Eval1/Tema 1/Simulacros de exam/copiadesefuridad.py
+++ Evaluaciones/Eval1/Tema 1/Simulacros de exam/copiadesefuridad.py
@@ -1,11 +1,79 @@
 import mysql.connector
 try:
-    mysql.connector.connect(
+    conexion=mysql.connector.connect(
         host="localhost",
-        user="backupuser",
-        password="clave_segura",
-        database="mi_base"
+        user="appuser",
+        password="m1ClaveSegura!",
+        database="portafolio"
     )
+    cursor = conexion.cursor()
+
+    
     print("✅ Conexión exitosa!")
+    print("Hola tu redacta database portafolio")
+    while True:
+        cursor = conexion.cursor()
+        print("Escoge una opcion:")
+        print("1.-Insertar un cliente")
+        print("2.-Listar los clientes")
+        print("3.-Actualizar un cliente")
+        print("4.-Borrar un cliente")
+        opcion = int(input("Escoge una opcion:"))
+
+        if opcion == 1:
+            print("Escoge una opcion:")
+            print("1.Insertar en tabla de Categoria")
+            print("2.Insertar en tabla de Pieza")
+            opciondeinsetar = int(input("Escoge una opcion:"))
+            if opciondeinsetar == 1:
+                Id_cat = input("Id_cat INT: ")
+                titulo_c = input("titulo_c VARCHAR(255): ")
+                descripcion_c = input("descripcion_c VARCHAR(255): ")
+
+                cursor.execute(
+        '''INSERT INTO Categoria
+VALUES ('''+ Id_cat + ''',\'''' + titulo_c + '''\',\'''' + descripcion_c + '''\'
+);'''
+    )
+                conexion.commit()
+            elif opciondeinsetar == 2:
+                id_pieza = input("id_pieza INT: ")
+                titulo_p = input("titulo_p VARCHAR(255): ")
+                descripcion_p = input("descripcion_p VARCHAR(255): ")
+                imagen = input("imagen: ")
+                tiurltulo_p = input("tiurltulo_p: ")
+                id_categoria = input("id_categoria INT: ")
+
+                cursor.execute('''
+                    INSERT INTO Pieza
+                    VALUES (
+                    '''+id_pieza+''',
+                    "''' + titulo_p + '''",
+                    "''' + descripcion_p + '''",
+                    "''' + imagen + '''",
+                    "''' + tiurltulo_p + '''",
+                    '''+id_categoria+'''
+                    );
+                ''')
+                conexion.commit()
+            else:
+                print("Numero incorecta")
+        elif opcion == 2:
+            print("Listamos los clientes")
+            cursor.execute("SELECT * FROM CatPIEz")
+            resultados = cursor.fetchall()
+            for fila in resultados:
+                print(fila)
+        elif opcion == 3:
+            break
+    cursor.close() 
+    conexion.close()
+
+
+
+
 except Exception as e:
     print("❌ Error:", e)
+
+
+
