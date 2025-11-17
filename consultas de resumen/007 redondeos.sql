c4ae643f58257d042cd9e886a611e458da66d807
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-11-17 11:52:03 +0100

    Update 007 redondeos.sql

--- consultas de resumen/007 redondeos.sql
+++ consultas de resumen/007 redondeos.sql
@@ -6,5 +6,13 @@ SELECT nombre
 FROM clientes;
 
 SELECT
-    AVG(edad)
+    ROUND(AVG(edad))
+FROM clientes;
+
+SELECT
+    FLOOR(AVG(edad))
+FROM clientes;
+
+SELECT
+    CEIL(AVG(edad))
 FROM clientes;
