6cf18eddf2d753acb4bed1317a66f6c0a35cb6f2
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-11-24 11:16:41 +0100

    Create 007 endpoint principal.py

---
+++ Explicationes/Comsiones claves/007 endpoint principal.py
@@ -0,0 +1,52 @@
+import mysql.connector
+from flask import Flask, render_template, jsonify, g
+
+import json
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host="localhost",
+            user="clientetienda",
+            password="Tiendaclase123$",
+            database="tienda"
+        )
+    return g.db
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+@app.route("/")
+def raiz():
+    return render_template("index.html")
+
+# http://127.0.0.1:5000/clientes
+@app.route("/clientes")
+def clientes():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM clientes;")
+    filas = cursor.fetchall()
+    cursor.close()
+    return jsonify(filas)
+
+# http://127.0.0.1:5000/tablas
+@app.route("/tablas")
+def tablas():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SHOW TABLES;")
+    filas = cursor.fetchall()
+    cursor.close()
+
+    tablas = [fila[0] for fila in filas]
+    return jsonify(tablas)
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)  # ver punto 2
